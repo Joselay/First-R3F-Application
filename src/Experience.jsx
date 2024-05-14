325b8808e99fef78a3feded4a359aca728c55fc4
Joselay <tongmenglaysmae5@gmail.com>
2024-05-14 08:39:52 +0700

    ...

--- src/Experience.jsx
+++ src/Experience.jsx
@@ -1,51 +1,33 @@
-import { extend, useFrame, useThree } from "@react-three/fiber";
-import { useRef } from "react";
-import { OrbitControls } from "three/examples/jsm/controls/OrbitControls";
-import CustomObject from "./CustomObject";
+import { useThree, extend } from '@react-three/fiber'
+import { OrbitControls } from 'three/examples/jsm/controls/OrbitControls.js'
 
-extend({ OrbitControls });
+extend({ OrbitControls })
 
-const Experience = () => {
-  const { camera, gl } = useThree();
-  const cubeRef = useRef();
-  const groupRef = useRef();
+export default function Experience()
+{
+    const { camera, gl } = useThree()
 
-  useFrame((state, delta) => {
-    cubeRef.current.rotation.y += delta;
-  });
+    return <>
 
-  return (
-    <>
-      <orbitControls args={[camera, gl.domElement]} />
+        <orbitControls args={ [ camera, gl.domElement ] } />
 
-      <directionalLight position={[1, 2, 3]} intensity={1.5} />
-      <ambientLight intensity={0.5} />
+        <directionalLight position={ [ 1, 2, 3 ] } intensity={ 1.5 } />
+        <ambientLight intensity={ 0.5 } />
 
-      <group ref={groupRef}>
-        <mesh position-x={-2}>
-          <sphereGeometry />
-          <meshStandardMaterial color="orange" />
+        <mesh position-x={ - 2 }>
+            <sphereGeometry />
+            <meshStandardMaterial color="orange" />
         </mesh>
 
-        <mesh
-          ref={cubeRef}
-          rotation-y={Math.PI * 0.25}
-          position-x={2}
-          scale={1.5}
-        >
-          <boxGeometry />
-          <meshStandardMaterial color="mediumpurple" />
+        <mesh position-x={ 2 } scale={ 1.5 }>
+            <boxGeometry />
+            <meshStandardMaterial color="mediumpurple" />
         </mesh>
-      </group>
 
-      <mesh position-y={-1} rotation-x={-Math.PI * 0.5} scale={10}>
-        <planeGeometry />
-        <meshStandardMaterial color="greenyellow" />
-      </mesh>
+        <mesh position-y={ - 1 } rotation-x={ - Math.PI * 0.5 } scale={ 10 }>
+            <planeGeometry />
+            <meshStandardMaterial color="greenyellow" />
+        </mesh>
 
-      <CustomObject />
     </>
-  );
-};
-
-export default Experience;
+}
