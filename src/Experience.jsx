208951034162cfaba7ff294e9f6bdac5ea9ccd77
Joselay <tongmenglaysmae5@gmail.com>
2024-05-14 17:48:04 +0700

    ...

--- src/Experience.jsx
+++ src/Experience.jsx
@@ -1,33 +1,34 @@
-import { useThree, extend } from '@react-three/fiber'
-import { OrbitControls } from 'three/examples/jsm/controls/OrbitControls.js'
+import {
+  OrbitControls,
+  PivotControls,
+  TransformControls,
+} from "@react-three/drei";
 
-extend({ OrbitControls })
+export default function Experience() {
+  return (
+    <>
+      <OrbitControls makeDefault />
+      <directionalLight position={[1, 2, 3]} intensity={1.5} />
+      <ambientLight intensity={0.5} />
 
-export default function Experience()
-{
-    const { camera, gl } = useThree()
-
-    return <>
-
-        <orbitControls args={ [ camera, gl.domElement ] } />
-
-        <directionalLight position={ [ 1, 2, 3 ] } intensity={ 1.5 } />
-        <ambientLight intensity={ 0.5 } />
-
-        <mesh position-x={ - 2 }>
-            <sphereGeometry />
-            <meshStandardMaterial color="orange" />
+      <PivotControls anchor={[0, 0, 0]} depthTest={false}>
+        <mesh position-x={-2}>
+          <sphereGeometry />
+          <meshStandardMaterial color="orange" />
         </mesh>
+      </PivotControls>
 
-        <mesh position-x={ 2 } scale={ 1.5 }>
-            <boxGeometry />
-            <meshStandardMaterial color="mediumpurple" />
+      <TransformControls position-x={2}>
+        <mesh scale={1.5}>
+          <boxGeometry />
+          <meshStandardMaterial color="mediumpurple" />
         </mesh>
+      </TransformControls>
 
-        <mesh position-y={ - 1 } rotation-x={ - Math.PI * 0.5 } scale={ 10 }>
-            <planeGeometry />
-            <meshStandardMaterial color="greenyellow" />
-        </mesh>
-
+      <mesh position-y={-1} rotation-x={-Math.PI * 0.5} scale={10}>
+        <planeGeometry />
+        <meshStandardMaterial color="greenyellow" />
+      </mesh>
     </>
+  );
 }
